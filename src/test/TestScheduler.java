2bc76b6450b620d09d436cb45b8ebab4fb0c32ba
Dorothy Tran <62575445+dorothytran@users.noreply.github.com>
2023-02-26 17:26:06 -0500

    Added Javadocs.

--- src/test/TestScheduler.java
+++ src/test/TestScheduler.java
@@ -9,8 +9,8 @@ import project.messageSystem.FloorMessageQueue;
 import project.scheduler.Scheduler;
 
 /**
- * @author elish
- *
+ * Scheduler Test Class.
+ * @author Elisha Catherasoo
  */
 class TestScheduler {
 	FloorMessageQueue fMQ = new FloorMessageQueue();
@@ -18,22 +18,33 @@ class TestScheduler {
 	
 	Scheduler scheduler = new Scheduler(fMQ, eMQ, "Scheduler");
 	
-	
+	/**
+	 * Test to check if the scheduler is initially dead.
+	 */
 	@Test
 	void testSchedulerIsDead() {
 		assertFalse(scheduler.getIsDead());
 	}	
 	
+	/**
+	 * Test to check the message queue message from the scheduler elevator subsystem.
+	 */
 	@Test
 	void testSchedulerElevatorSubSystemMessageQueue() {
 		assertEquals(scheduler.getElevatorSubSystemMessageQueue(), eMQ);
 	}	
 	
+	/**
+	 * Test to check the message queue message from the scheduler floor system.
+	 */
 	@Test
 	void testSchedulerFloorMessageQueue() {
 		assertEquals(scheduler.getFloorMessageQueue(), fMQ);
 	}	
 	
+	/**
+	 * Test to check the name of scheduler system.
+	 */
 	@Test
 	void testSchedulerSystemName() {
 		assertEquals(scheduler.getSystemName(), "Scheduler");
