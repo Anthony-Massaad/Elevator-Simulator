ed272c9d6d022301110b803e46b64891321af423
Dorothy Tran <62575445+dorothytran@users.noreply.github.com>
2023-02-26 17:23:39 -0500

    Added Javadocs.

--- src/test/TestFloor.java
+++ src/test/TestFloor.java
@@ -9,8 +9,8 @@ import project.simulationParser.*;
 import project.messageSystem.*;
 
 /**
- * @author elish
- *
+ * Floor Test Class.
+ * @author Elisha Catherasoo, Dorothy Tran
  */
 class TestFloor {
 	Parser parser = new Parser();
@@ -18,32 +18,49 @@ class TestFloor {
 	
 	Floor floor = new Floor(parser, fMQ, 0, "Floor[0]");
 	
-	
+	/**
+	 * Test to check if the floor is initially dead.
+	 */
 	@Test
 	void testFloorIsDead() {
 		assertFalse(floor.getIsDead());
 	}
 	
+	/**
+	 * Test to check the floor parser.
+	 */
 	@Test
 	void testFloorParser() {
 		assertEquals(floor.getParser(), parser);
 	}
 	
+	/**
+	 * Test to check if the message of the floor message queue is equal to the floor's message.
+	 */
 	@Test
 	void testFloorMessageQueue() {
 		assertEquals(floor.getMessageQueue(), fMQ);
 	}
 	
+	/**
+	 * Test to check the name of floor system.
+	 */
 	@Test
 	void testFloorSystemName() {
 		assertEquals(floor.getSystemName(), "Floor[0]");
 	}
 	
+	/**
+	 * Test to check if the initial current floor the elevator is on is floor 0.
+	 */
 	@Test
 	void testFloorFloorNumber() {
 		assertEquals(floor.getFloorNumber(), 0);
 	}
 	
+	/**
+	 * Test to check if the request from the floor has been sent to the message queue.
+	 */
 	@Test
 	void testFloorRequestSent() {
 		assertFalse(floor.getRequestSent());
