4be405d1ac1c4bd902095246720ddb8b69b13c0f
Dorothy Tran <62575445+dorothytran@users.noreply.github.com>
2023-02-26 17:16:30 -0500

    Added Javadocs.

--- src/test/TestElevator.java
+++ src/test/TestElevator.java
@@ -12,8 +12,8 @@ import project.elevatorSubSystem.Elevator;
 import project.messageSystem.Message;
 
 /**
+ * Elevator Test Class.
  * @author Elisha Catherasoo, Dorothy Tran
- *
  */
 class TestElevator {
 	private ConcurrentLinkedDeque<Message> responses;
@@ -21,41 +21,65 @@ class TestElevator {
 	Message message = new Message(new Date(), "Elevator has arrived");
 	Elevator Elevator = new Elevator("Elevator[0]", responses, 0);
 	
+	/**
+	 * Test to check if the elevator is initially dead.
+	 */
 	@Test
 	void testElevatorIsDead() {
 		assertFalse(Elevator.getIsDead());
 	}
 	
+	/**
+	 * Test to check the name of elevator system.
+	 */
 	@Test
 	void testElevatorSystemName() {
 		assertEquals(Elevator.getSystemName(), "Elevator[0]");
 	}
 	
+	/**
+	 * Test to check if the ConcurrentLinkedDeque of responses is initially empty.
+	 */
 	@Test
 	void testElevatorResponses() {
 		assertEquals(Elevator.getResponses(), null);
 	}
 	
+	/**
+	 * Test to check if the initial current floor the elevator is on is floor 0.
+	 */
 	@Test
 	void testElevatorCurrentFloor() {
 		assertEquals(Elevator.getCurrentFloor(), 0);
 	}
 	
+	/**
+	 * Test to check if the destination floor is initally floor 0.
+	 */
 	@Test
 	void testElevatorDestinationFloor() {
 		assertEquals(Elevator.getDestinationFloor(), 0);
 	}
 	
+	/**
+	 * Test to check if the initial state of the Elevator is IDLE.
+	 */
 	@Test
 	void testElevatorState() {
 		assertEquals(Elevator.getElevatorState(), ElevatorState.IDLE);
 	}
 	
+	/**
+	 * Test to check if the ConcurrentLinkedDeque of requests is initially empty.
+	 */
 	@Test
 	void testElevatorRequests() {
 		assertTrue(Elevator.getRequests().isEmpty());
 	}
 	
+	/**
+	 * Test to check if requests are added into the ConcurrentLinkedDeque of requests.
+	 */
 	@Test
 	void testElevatorAddRequests() {
 		assertEquals(Elevator.getRequests().size(), 0); //check if initially empty
