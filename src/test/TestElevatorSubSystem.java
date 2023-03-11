bd5f94e2bae3437bbfde6914663f4c1a288bedff
Dorothy Tran <62575445+dorothytran@users.noreply.github.com>
2023-03-11 17:19:47 -0500

    Added Javadocs.

--- src/test/TestElevatorSubSystem.java
+++ src/test/TestElevatorSubSystem.java
@@ -14,16 +14,25 @@ import project.elevatorImpl.ElevatorSubSystem;
 class TestElevatorSubSystem {
 	ElevatorSubSystem eSubSystem = new ElevatorSubSystem(SimulationConstants.ELEVATOR_MANAGER_PORT, "ElevatorSubSystem");
 	
+	/**
+	 * Test method to check if the Elevator SubSystem's intital responses are empty.
+	 */
 	@Test
 	void testElevatorSubSystemGetElevatorResponses() {
 		assertTrue(eSubSystem.getElevatorResponses().isEmpty());
 	}
 	
+	/**
+	 * Test method for the getIsDead() getter method.
+	 */
 	@Test
 	void testElevatorSubSystemGetIsDead() {
 		assertEquals(eSubSystem.getIsDead(), false);
 	}
 	
+	/**
+	 * Test method to check the number of elevators of the Elevator SubSystem.
+	 */
 	@Test
 	void testElevatorSubSystemGetElevators() {
 		assertEquals(eSubSystem.getElevators().length, 4);
