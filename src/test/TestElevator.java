127b6b9ed34ac7b1fb2d2c234834c54cb71172be
Dorothy Tran <62575445+dorothytran@users.noreply.github.com>
2023-03-11 17:18:09 -0500

    Added Javadocs

--- src/test/TestElevator.java
+++ src/test/TestElevator.java
@@ -26,15 +26,19 @@ class TestElevator {
 	// Message message = new Message(new Date(), "Elevator has arrived");
 	ConcurrentLinkedDeque<Message> responsesConcurrentLinkedDeque = new ConcurrentLinkedDeque<>();
 	Elevator Elevator = new Elevator(0, "Elevator[0]", responses);
-		
-	// ADD GETTERS TESTING
 	
+	/**
+	 * Test method to check if the Elevator's State is IDLE when checking the elevator's message.
+	 */
 	@Test
 	void testElevatorCheckMessage() {
 		Elevator.checkMessage();
 		assertEquals(Elevator.getState(), ElevatorState.IDLE);
 	}
 	
+	/**
+	 * Test method to check if the Elevator's state is CLOSE_DOOR when handleOpenDoor() is invoked.
+	 */
 	@Test
 	void testElevatorHandleOpenDoor() {
 		try {
@@ -45,7 +49,9 @@ class TestElevator {
 		}
 	}
 	
-	
+	/**
+	 * Test method to check if the Elevator's state if IDLE when handleCloseDoor() is invoked.
+	 */
 	@Test
 	void testElevatorHandleCloseDoor() {
 		try {
@@ -56,7 +62,9 @@ class TestElevator {
 		}
 	}
 	
-	
+	/**
+	 * Test method to check if the Elevator's state is IDLE when handleMoving() is invoked.
+	 */
 	@Test
 	void testElevatorHandleMoving() {
 		try {
@@ -67,6 +75,9 @@ class TestElevator {
 		}
 	}
 	
+	/**
+	 * Test method to check if the Upcoming Buttons are added to the HashMap of buttons.
+	 */
 	@Test
 	void testElevatorAddUpcomingButtons() {
 		ArrayList<Integer> buttons = new ArrayList<>();
@@ -78,22 +89,34 @@ class TestElevator {
 		assertEquals(Elevator.getFloorInputButtons(), floorInputButtonsHashMap);
 	}
 	
+	/**
+	 * Test method to check the Elevator responses.
+	 */
 	@Test
 	void testElevatorGetResponses() {
 		assertEquals(Elevator.getResponses(), responses);
 	}
 	
+	/**
+	 * Test method to check if the Elevator requests are initially empty.
+	 */
 	@Test
 	void testElevatorGetRequests() {
 		assertTrue(Elevator.getRequests().isEmpty());
 	}
 	
+	/**3
+	 * Test method to get the Elevator destinations.
+	 */
 	@Test
 	void testElevatorGetDestinations() {
 		ArrayList<Integer>destinations = new ArrayList<>();
 		assertEquals(Elevator.getDestinations(), destinations);
 	}
 	
+	/**
+	 * Test method to get the Elevator intial statuses.
+	 */
 	@Test
 	void testElevatorGetElevatorStatus() {
 		assertEquals(Elevator.getElevatorStatus().getMotorDirection(), MotorDirection.IDLE);
@@ -101,26 +124,41 @@ class TestElevator {
 		assertEquals(Elevator.getElevatorStatus().getNextDestination(), 0);
 	}
 	
+	/**
+	 * Test method to check the Elevator's intial state.
+	 */
 	@Test
 	void testElevatorGetState() {
 		assertEquals(Elevator.getState(), ElevatorState.IDLE);
 	}
 	
+	/**
+	 * Test method to check the size of the Elevator lamps.
+	 */
 	@Test
 	void testElevatorGetLamps() {
 		assertEquals(Elevator.getLamps().length, 22);
 	}
 	
+	/**
+	 * Test method to check the Elevator's system name.
+	 */
 	@Test
 	void testElevatorGetSystemName() {
 		assertEquals(Elevator.getSystemName(), "Elevator[0]");
 	}
 	
+	/**
+	 * Test method to get the the Elevator Id.
+	 */
 	@Test
 	void testElevatorGetId() {
 		assertEquals(Elevator.getId(), 0);
 	}
 	
+	/**
+	 * Test method to check if the floor input buttons are added to the HashMap.
+	 */
 	@Test
 	void testElevatorGetFloorInputButtons() {
 		HashMap<Integer, ArrayList<Integer>> floorInputButtons = new HashMap<>();
