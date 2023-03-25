f0d3cf07223e7fc73ba3dfb7345f35daec3d0398
elisha <elisha123jc@gmail.com>
2023-03-25 14:24:24 -0400

    fixed Elevator and Scheduler Testing

--- src/test/TestElevator.java
+++ src/test/TestElevator.java
@@ -1,205 +1,161 @@
-// package test;
+package test;
 
-// import static org.junit.jupiter.api.Assertions.*;
-// import java.util.concurrent.ConcurrentLinkedDeque;
-// import java.util.ArrayList;
-// import java.util.Date;
-// import java.util.HashMap;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
-// import project.constants.ElevatorState;
-// import project.constants.MotorDirection;
-// import project.messageSystem.Message;
-// import project.messageSystem.messages.ArrivalMessage;
-// import project.messageSystem.messages.MoveToMessage;
-// import project.elevatorImpl.Elevator;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import project.constants.MotorDirection;
+import project.messageSystem.Message;
+import project.elevatorImpl.Elevator;
 
-// /**
-//  * Elevator Test Class.
-//  * @author Elisha Catherasoo, Dorothy Tran
-//  */
-// class TestElevator {
+/**
+ * Elevator Test Class.
+ * @author Elisha Catherasoo, Dorothy Tran
+ */
+class TestElevator {
 	
-// 	private Elevator Elevator; 
-// 	private ConcurrentLinkedDeque<Message> responses;
-// 	@BeforeEach
-// 	public void init(){
-// 		System.out.println("Starting Up");
-// 		responses = new ConcurrentLinkedDeque<>();
-// 		// Message message = new Message(new Date(), "Elevator has arrived");
-// 		Elevator = new Elevator(0, "Elevator[0]", responses);
-// 	}
+	private Elevator Elevator; 
+	private ConcurrentLinkedDeque<Message> responses;
+	@BeforeEach
+	public void init(){
+		System.out.println("Starting Up");
+		responses = new ConcurrentLinkedDeque<>();
+		// Message message = new Message(new Date(), "Elevator has arrived");
+		Elevator = new Elevator(0, "Elevator[0]", responses);
+	}
 	
-// 	/**
-// 	 * Test method to check if the Elevator's State is IDLE when checking the elevator's message.
-// 	 */
-// 	@Test
-// 	void testElevatorCheckMessage() {
-// 		Elevator.checkMessage();
-// 		assertEquals(Elevator.getState(), ElevatorState.IDLE);
-// 	}
+	@Test
+	void testElevatorSort() {
+		ArrayList<Integer> tempDest = new ArrayList<>();
+		tempDest.add(10);
+		tempDest.add(7);
+		tempDest.add(3);
+		Elevator.setDestinations(tempDest);
+		Elevator.sort(false);
+		
+		ArrayList<Integer> destinations = new ArrayList<>();
+		destinations.add(3);
+		destinations.add(7);
+		destinations.add(10);
+		
+		assertEquals(Elevator.getDestinations(), destinations);
+	}
 	
-// 	/**
-// 	 * Test method to check if the Elevator's state is CLOSE_DOOR when handleOpenDoor() is invoked.
-// 	 */
-// 	@Test
-// 	void testElevatorHandleOpenDoor() {
-// 		try {
-// 			Elevator.handleOpenDoor();
-// 			assertEquals(Elevator.getState(), ElevatorState.CLOSE_DOOR);
-// 		} catch (InterruptedException e) {
-// 			e.printStackTrace();
-// 		}
-// 	}
+	/**
+	 * Test method to check if the Elevator's state is CLOSE_DOOR when handleOpenDoor() is invoked.
+	 */
+	@Test
+	void testElevatorUpdateMotorStatus() {
+		Elevator.updateMotorStatus();
+		
+		assertEquals(Elevator.getElevatorStatus().getMotorDirection(), MotorDirection.DOWN);
+		assertFalse(Elevator.getDirectionLampUp());
+		assertTrue(Elevator.getDirectionLampDown());
+	}
 	
-// 	/**
-// 	 * Test method to check if the Elevator's state if IDLE when handleCloseDoor() is invoked.
-// 	 */
-// 	@Test
-// 	void testElevatorHandleCloseDoor() {
-// 		try {
-// 			Elevator.handleCloseDoor();
-// 			assertEquals(Elevator.getState(), ElevatorState.IDLE);
-// 		} catch (InterruptedException e) {
-// 			e.printStackTrace();
-// 		}
-// 	}
+	/**
+	 * Test method to check if the Upcoming Buttons are added to the HashMap of buttons.
+	 */
+	@Test
+	void testElevatorAppendButtonsToExistingList() {
+		ArrayList<Integer> curr = new ArrayList<>();
+		curr.add(1);
+		curr.add(3);
+		curr.add(7);
+		
+		ArrayList<Integer> toAdd = new ArrayList<>();
+		toAdd.add(12);
+		toAdd.add(5);
+		
+		ArrayList<Integer> btnsList = new ArrayList<>();
+		btnsList.add(1);
+		btnsList.add(3);
+		btnsList.add(7);
+		btnsList.add(12);
+		btnsList.add(5);
+		
+		assertEquals(Elevator.appendButtonsToExistingList(curr, toAdd), btnsList);
+	}
 	
-// 	/**
-// 	 * Test method to check if the Elevator's state is IDLE when handleMoving() is invoked.
-// 	 */
-// 	@Test
-// 	void testElevatorHandleMovingWithNoMoveToMessage() {
-// 		try {
-// 			Elevator.handleMoving();
-// 			assertEquals(Elevator.getState(), ElevatorState.IDLE);
-// 		} catch (InterruptedException e) {
-// 			e.printStackTrace();
-// 		}
-// 	}
-
-// 	@Test
-// 	void testElevatorHandleMovingWithMoveMessage() throws InterruptedException{
-// 		int dest = 5;
-// 		Elevator.addRequest(new MoveToMessage(new Date(), dest, new ArrayList<>()));
-// 		Elevator.checkMessage();
-// 		assertEquals(ElevatorState.MOVING, Elevator.getState());
-// 		assertEquals(dest, Elevator.getElevatorStatus().getNextDestination());
-// 		if (dest > Elevator.getElevatorStatus().getCurrentFloor()){
-// 			for (int i = Elevator.getElevatorStatus().getCurrentFloor(); i < dest; i++ ){
-// 				Elevator.handleMoving();
-// 			}
-// 		}else{
-// 			for (int i = Elevator.getElevatorStatus().getCurrentFloor(); i > dest; i-- ){
-// 				Elevator.handleMoving();
-// 			}
-// 		}
-// 		assertTrue(Elevator.getElevatorStatus().getCurrentFloor() == dest);
-// 		assertEquals(ElevatorState.OPEN_DOOR, Elevator.getState());
-// 	}
+	/**
+	 * Test method to check if the Upcoming Buttons are added to the HashMap of buttons.
+	 */
+	@Test
+	void testElevatorAddUpcomingButtons() {
+		ArrayList<Integer> buttons = new ArrayList<>();
+		Elevator.addUpcomingButtons(2, buttons);
+		HashMap<Integer, ArrayList<Integer>> floorInputButtonsHashMap = new HashMap<>();
+		floorInputButtonsHashMap.put(2, buttons);
+		assertEquals(Elevator.getFloorInputButtons().size(), 0);
+	}
 	
-// 	/**
-// 	 * Test method to check if the Upcoming Buttons are added to the HashMap of buttons.
-// 	 */
-// 	@Test
-// 	void testElevatorAddUpcomingButtons() {
-// 		ArrayList<Integer> buttons = new ArrayList<>();
-// 		Elevator.addUpcomingButtons(2, buttons);
-// 		HashMap<Integer, ArrayList<Integer>> floorInputButtonsHashMap = new HashMap<>();
-// 		floorInputButtonsHashMap.put(2, buttons);
-// 		assertEquals(Elevator.getFloorInputButtons().size(), 0);
-// 	}
+	/**
+	 * Test method to check if the floor input buttons are added to the HashMap.
+	 */
+	@Test
+	void testElevatorGetFloorInputButtons() {
+		HashMap<Integer, ArrayList<Integer>> floorInputButtons = new HashMap<>();
+		assertEquals(Elevator.getFloorInputButtons(), floorInputButtons);
+	}
 	
-// 	/**
-// 	 * Test method to check the Elevator responses.
-// 	 */
-// 	@Test
-// 	void testElevatorGetResponses() {
-// 		assertEquals(Elevator.getResponses(), responses);
-// 	}
+	/**
+	 * Test method to check the Elevator responses.
+	 */
+	@Test
+	void testElevatorGetResponses() {
+		assertEquals(Elevator.getResponses(), responses);
+	}
 	
-// 	/**
-// 	 * Test method to check if the Elevator requests are initially empty.
-// 	 */
-// 	@Test
-// 	void testElevatorGetRequests() {
-// 		assertTrue(Elevator.getRequests().isEmpty());
-// 	}
+	/**
+	 * Test method to check if the Elevator requests are initially empty.
+	 */
+	@Test
+	void testElevatorGetRequests() {
+		assertTrue(Elevator.getRequests().isEmpty());
+	}
 	
-// 	/**3
-// 	 * Test method to get the Elevator destinations.
-// 	 */
-// 	@Test
-// 	void testElevatorGetDestinations() {
-// 		ArrayList<Integer>destinations = new ArrayList<>();
-// 		assertEquals(Elevator.getDestinations(), destinations);
-// 	}
+	/**3
+	 * Test method to get the Elevator destinations.
+	 */
+	@Test
+	void testElevatorGetDestinations() {
+		ArrayList<Integer>destinations = new ArrayList<>();
+		assertEquals(Elevator.getDestinations(), destinations);
+	}
 	
-// 	/**
-// 	 * Test method to get the Elevator intial statuses.
-// 	 */
-// 	@Test
-// 	void testElevatorGetElevatorStatus() {
-// 		assertEquals(Elevator.getElevatorStatus().getMotorDirection(), MotorDirection.IDLE);
-// 		assertEquals(Elevator.getElevatorStatus().getNumberOfPassengers(), 0);
-// 		assertEquals(Elevator.getElevatorStatus().getNextDestination(), 0);
-// 	}
+	/**
+	 * Test method to get the Elevator intial statuses.
+	 */
+	@Test
+	void testElevatorGetElevatorStatus() {
+		assertEquals(Elevator.getElevatorStatus().getMotorDirection(), MotorDirection.IDLE);
+		assertEquals(Elevator.getElevatorStatus().getNumberOfPassengers(), 0);
+		assertEquals(Elevator.getElevatorStatus().getNextDestination(), 0);
+	}
 	
-// 	/**
-// 	 * Test method to check the Elevator's intial state.
-// 	 */
-// 	@Test
-// 	void testElevatorGetState() {
-// 		assertEquals(Elevator.getState(), ElevatorState.IDLE);
-// 	}
+	/**
+	 * Test method to check the size of the Elevator lamps.
+	 */
+	@Test
+	void testElevatorGetLamps() {
+		assertEquals(Elevator.getLamps().length, 22);
+	}
 	
-// 	/**
-// 	 * Test method to check the size of the Elevator lamps.
-// 	 */
-// 	@Test
-// 	void testElevatorGetLamps() {
-// 		assertEquals(Elevator.getLamps().length, 22);
-// 	}
+	/**
+	 * Test method to check the Elevator's system name.
+	 */
+	@Test
+	void testElevatorGetSystemName() {
+		assertEquals(Elevator.getSystemName(), "Elevator[0]");
+	}
 	
-// 	/**
-// 	 * Test method to check the Elevator's system name.
-// 	 */
-// 	@Test
-// 	void testElevatorGetSystemName() {
-// 		assertEquals(Elevator.getSystemName(), "Elevator[0]");
-// 	}
-	
-// 	/**
-// 	 * Test method to get the the Elevator Id.
-// 	 */
-// 	@Test
-// 	void testElevatorGetId() {
-// 		assertEquals(Elevator.getId(), 0);
-// 	}
-	
-// 	/**
-// 	 * Test method to check if the floor input buttons are added to the HashMap.
-// 	 */
-// 	@Test
-// 	void testElevatorGetFloorInputButtons() {
-// 		HashMap<Integer, ArrayList<Integer>> floorInputButtons = new HashMap<>();
-// 		assertEquals(Elevator.getFloorInputButtons(), floorInputButtons);
-// 	}
-
-// 	// Error cases 
-// 	@Test 
-// 	void testFeedElevatorRandomMessage(){
-// 		ArrivalMessage message = new ArrivalMessage(new Date(), 0, MotorDirection.DOWN);
-// 		Elevator.addRequest(message);
-// 		boolean result = Elevator.checkMessage();
-// 		assertFalse(result);
-// 		assertEquals(ElevatorState.IDLE, Elevator.getState());
-// 	}
-
-// 	@Test
-// 	void FeedElevatorNoMessage(){
-// 		boolean result = Elevator.checkMessage();
-// 		assertFalse(result);
-// 		assertEquals(ElevatorState.IDLE, Elevator.getState());
-// 	}
-// }
+	/**
+	 * Test method to get the the Elevator Id.
+	 */
+	@Test
+	void testElevatorGetId() {
+		assertEquals(Elevator.getId(), 0);
+	}
+}
--- src/test/TestScheduler.java
+++ src/test/TestScheduler.java
@@ -1,105 +1,86 @@
-// package test;
+package test;
 
-// import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-// import java.io.IOException;
-// import java.util.ArrayList;
-// import java.util.Date;
-// import java.util.concurrent.ConcurrentHashMap;
+import java.awt.Taskbar.State;
+import java.awt.im.InputMethodRequests;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.concurrent.ConcurrentHashMap;
 
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-// import project.constants.MotorDirection;
-// import project.constants.SchedulerState;
-// import project.constants.SimulationConstants;
-// import project.elevatorImpl.ElevatorStatus;
-// import project.messageSystem.messages.FloorRequestElevator;
-// import project.messageSystem.messages.MoveToMessage;
-// import project.schedulerImpl.Scheduler;
+import project.constants.MotorDirection;
+import project.constants.SimulationConstants;
+import project.elevatorImpl.ElevatorStatus;
+import project.messageSystem.Message;
+import project.messageSystem.messages.FloorRequestElevator;
+import project.messageSystem.messages.MoveToMessage;
+import project.schedulerImpl.Scheduler;
+import project.statesImpl.SchedulerStates.SchedulerIdleState;
+import project.statesImpl.SchedulerStates.SchedulerProcessFloorState;
 
-// /**
-//  * Scheduler Test Class.
-//  * @author Elisha Catherasoo
-//  */
-// class TestScheduler {
+/**
+ * Scheduler Test Class.
+ * @author Elisha Catherasoo, Dorothy Tran
+ */
+class TestScheduler {
 
-// 	private Scheduler scheduler;
-// 	@BeforeEach
-// 	public void init(){
-// 		scheduler = new Scheduler(SimulationConstants.SCHEDULER_PORT, "Scheduler");
-// 	}
+	private Scheduler scheduler;
+	@BeforeEach
+	public void init(){
+		scheduler = new Scheduler(SimulationConstants.SCHEDULER_PORT, "Scheduler");
+	}
 	
-// 	/**
-// 	 * Test method to check the Elevator Statuses.
-// 	 */
-// 	@Test
-// 	void testSchedulerElevatorStatuses() {
-// 		ConcurrentHashMap<Integer, ElevatorStatus> elevatorStatuses = new ConcurrentHashMap<>();
-// 		assertEquals(scheduler.getElevatorStatuses(), elevatorStatuses);
-// 	}
+	/**
+	 * Test method to check the Elevator Statuses.
+	 */
+	@Test
+	void testSchedulerElevatorStatuses() {
+		ConcurrentHashMap<Integer, ElevatorStatus> elevatorStatuses = new ConcurrentHashMap<>();
+		assertEquals(scheduler.getElevatorStatuses(), elevatorStatuses);
+	}
 	
-// 	/**
-// 	 * Test method for the selectElevator() method.
-// 	 */
-// 	@Test
-// 	void testSchedulerSelectElevator() {
-// 		for (int i = 0; i < SimulationConstants.NUM_OF_ELEVATORS; i++){
-// 			scheduler.addElevatorStatus(i, new ElevatorStatus());
-//         }
-// 		assertEquals(scheduler.selectElevator(6, MotorDirection.UP), 0);
-// 	}
+	/**
+	 * Test method for the selectElevator() method.
+	 */
+	@Test
+	void testSchedulerSelectElevator() {
+		for (int i = 0; i < SimulationConstants.NUM_OF_ELEVATORS; i++){
+			scheduler.addElevatorStatus(i, new ElevatorStatus());
+        }
+		assertEquals(scheduler.selectElevator(6, MotorDirection.UP), 0);
+	}
 
-// 	@Test
-// 	void testSchedulerInitialState(){
-// 		assertEquals(SchedulerState.IDLE, scheduler.getState());
-// 	}
+	@Test
+	void testSchedulerStateValidMessage(){
+		scheduler.setReceivedMessage(new FloorRequestElevator(new Date(), 0, MotorDirection.IDLE, new ArrayList<>()));
+		scheduler.setCurrentState(scheduler.getProcessFloorState());
+		project.statesImpl.State state = scheduler.getCurrentState();
+		
+		assertTrue(state instanceof SchedulerProcessFloorState);
+	}
 
-// 	@Test
-// 	void testSchedulerStateValidMessage(){
-// 		scheduler.setReceivedMessage(new FloorRequestElevator(new Date(), 0, MotorDirection.IDLE, new ArrayList<>()));
-// 		scheduler.checkState();
-// 		assertEquals(SchedulerState.PROCESSING_FLOOR, scheduler.getState());
-// 	}
-
-// 	@Test
-// 	void testSchedulerAftermathOfValiMessage() throws IOException, InterruptedException{
-// 		for (int i = 0; i < SimulationConstants.NUM_OF_ELEVATORS; i++){
-// 			scheduler.addElevatorStatus(i, new ElevatorStatus());
-//         }
-// 		scheduler.setReceivedMessage(new FloorRequestElevator(new Date(), 0, MotorDirection.IDLE, new ArrayList<>()));
-// 		scheduler.checkState();
-// 		assertEquals(SchedulerState.PROCESSING_FLOOR, scheduler.getState());
-// 		boolean result = scheduler.processMessage();
-// 		assertTrue(result);
-// 	}
-
-// 	@Test
-// 	void testSchedulerAftermathStateOfValiMessage() throws IOException, InterruptedException{
-// 		for (int i = 0; i < SimulationConstants.NUM_OF_ELEVATORS; i++){
-// 			scheduler.addElevatorStatus(i, new ElevatorStatus());
-//         }
-// 		scheduler.setReceivedMessage(new FloorRequestElevator(new Date(), 0, MotorDirection.IDLE, new ArrayList<>()));
-// 		scheduler.checkState();
-// 		assertEquals(SchedulerState.PROCESSING_FLOOR, scheduler.getState());
-// 		boolean result = scheduler.processMessage();
-// 		assertTrue(result);
-// 		assertEquals(SchedulerState.IDLE, scheduler.getState());
-// 		assertNull(scheduler.getReceviedMessage());
-// 	}
-
-// 	//  Error Casess / failure cases
-// 	@Test 
-// 	void testSchedulerNullMessage() throws IOException, InterruptedException{
-// 		boolean result = scheduler.processMessage();
-// 		assertFalse(result);
-// 	}
-
-// 	@Test
-// 	void testSchedulerInvalidMessage() throws IOException, InterruptedException{
-// 		scheduler.setReceivedMessage(new MoveToMessage(new Date(), 0, new ArrayList<>()));
-// 		boolean result = scheduler.processMessage();
-// 		assertFalse(result);
-// 	}
+	@Test
+	void testSchedulerReset(){
+		scheduler.reset();
+		
+		assertNull(scheduler.getReceviedMessage());
+	}
 	
-// }
+	@Test
+	void testSchedulerAftermathOfValiMessage() throws IOException, InterruptedException{
+		for (int i = 0; i < SimulationConstants.NUM_OF_ELEVATORS; i++){
+			scheduler.addElevatorStatus(i, new ElevatorStatus());
+        }
+		scheduler.setReceivedMessage(new FloorRequestElevator(new Date(), 0, MotorDirection.IDLE, new ArrayList<>()));
+		scheduler.setCurrentState(scheduler.getProcessFloorState());
+		project.statesImpl.State state = scheduler.getCurrentState();
+		
+		assertTrue(state instanceof SchedulerProcessFloorState);
+		scheduler.setCurrentState(state.handleState());
+		assertTrue(scheduler.getCurrentState() instanceof SchedulerIdleState);
+	}
+}
